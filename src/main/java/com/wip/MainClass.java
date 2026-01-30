e4c4ec2a5a487c402d87514984f735af594774b4
Sri Dikshitha <Jan1user248@rpsconsulting.in>
2026-01-30 12:10:02 +0530

    Updated MainClass

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -8,24 +8,17 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        MainClass mc = new MainClass();
-        System.out.println(mc.add(10, 20));
+        System.out.println(new MainClass().add(10, 20));
         System.out.println("Java For Docker!!!! This code is pulled from WIP GitHub Repository");
         System.out.println("This is the updated code after adding Jenkinsfile to the repository");
 
-     
-        Thread keepAliveThread = new Thread(() -> {
-            try {
-                while (true) {
-                    System.out.println("Application is running...");
-                    Thread.sleep(5000);
-                }
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted, shutting down...");
+        try {
+            while (true) {
+                System.out.println("Application is running...");
+                Thread.sleep(6000);
             }
-        });
-
-        keepAliveThread.setDaemon(false); 
-        keepAliveThread.start();
+        } catch (InterruptedException e) {
+            System.out.println("Application stopped");
+        }
     }
 }
